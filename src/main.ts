16fc03f5824022d272e9baf7383c0ed9af088606
quentin.bacq <quentin.bacq@epitech.eu>
2019-09-20 16:55:55 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, () => console.log(process.env.PORT));
+  await app.listen(process.env.PORT || 3000, () => {
+    // tslint:disable-next-line:no-console
+    console.log(process.env.PORT);
+  });
 }
 bootstrap();
